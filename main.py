58bc78b2dae8744dde195c273eb49012842f4993
THproger13 <thphysics@naver.com>
2023-11-05 23:42:46 +0900

    가상 결제 데이터 생성 함수 스크립트 파일 (호텔 예약 사이트 db에 맞게 리팩토링 예정), main.py엔드 포인트 추가

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
-
+# 기존 엔드포인트
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+# 새로운 엔드포인트: transactions.json 파일로부터 데이터를 읽어서 반환
+@app.get("/transactions")
+def read_transactions():
+    with open('data/transactions.json', 'r') as f:  # 파일 경로 확인 필요
+        transactions = json.load(f)
+    return transactions
